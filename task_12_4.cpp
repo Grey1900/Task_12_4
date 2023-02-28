45dc06f2fe2b624b3604da5ccb330a2f13387847
TimkyWimky <36457305+Grey1900@users.noreply.github.com>
2023-02-28 16:53:02 +0400

    Initial commit

---
+++ task_12_4.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+void _sort(int nums[], int n)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i%2; j < n-1; j += 2)
+        {
+            if (nums[j] > nums[j+1])
+            {
+                swap(nums[j], nums[j+1]);
+            }
+        }
+    }
+}
+
+void dubles(int nums[], int n)
+{
+    int j = 0;
+    int dubles[n];
+
+    for (int i = 0; i < n-1; i++)
+    {
+        if (nums[i] == nums[i+1])
+        {
+            dubles[j] = nums[i];
+            j++;
+        }
+    }
+
+    cout << "Repeated numbers: ";
+    for (int i = 0; i < j; i++)
+    {
+        cout << dubles[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    const int n = 5;
+    int nums[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> nums[i];
+    }
+
+    _sort(nums, n);
+
+    cout << "Minimum number: " << nums[0] << endl;
+
+    dubles(nums, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
